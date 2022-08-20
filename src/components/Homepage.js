bdd2ee778b42400c209e650128dd338c3ed9f3bb
Muhammad Saad Atique <msaadatiq@gmail.com>
2022-08-19 21:50:24 -0400

    added readme

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -1,27 +1,148 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { signOut, onAuthStateChanged } from 'firebase/auth';
-import { auth } from "../firebase.js"
+import { auth, db } from "../firebase.js"
 import { useNavigate } from 'react-router-dom';
+import { uid } from 'uid';
+import { set, ref, onValue, remove, update } from 'firebase/database';
+import './homepage.css'
+import AddIcon from "@mui/icons-material/Add";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteIcon from "@mui/icons-material/Delete";
+import ExitToAppTwoToneIcon from '@mui/icons-material/ExitToAppTwoTone';
+// import LogoutIcon from '@mui/icons-material/Logout';
+import CheckIcon from '@mui/icons-material/Check';
 export default function Homepage() {
     const navigate = useNavigate();
-
+    const [todo, setTodo] = useState('');
+    const [todos, setTodos] = useState([]);
+    const [isEdit, setIsEdit] = useState(false);
+    const [tempUidd, setTempUidd] = useState('')
+    // if user is not signed in they can't go to homepage/todo list page
     useEffect(() => {
         auth.onAuthStateChanged((user) => {
-            if (!user) {
+            if (user) {
+                onValue(ref(db, `/${auth.currentUser.uid}`), snapshot => {
+                    setTodos([]);
+                    const data = snapshot.val();
+                    if (data !== null) {
+                        Object.values(data).map((todo) => {
+                            setTodos((oldArray) => [...oldArray, todo]);
+                        });
+                    }
+                });
+            }
+            else if (!user) {
                 navigate('/')
             }
         })
     }, [])
 
     const handleSignOut = () => {
-        signOut(auth).then(() => {
-            navigate("/");
-        }).catch((err) => { alert(err.message); });
+        signOut(auth)
+            .then(() => {
+                navigate("/");
+            })
+            .catch((err) => {
+                alert(err.message);
+            });
     };
+
+    // add
+    const writeToDatabase = () => {
+        const uidd = uid();
+        set(ref(db, `/${auth.currentUser.uid}/${uidd}`), {
+            todo: todo,
+            uidd: uidd
+        });
+
+        setTodo("");
+    };
+
+    const handleDelete = (uid) => {
+        remove(ref(db, `/${auth.currentUser.uid}/${uid}`));
+    };
+
+
+
+    // update
+    const handleUpdate = (todo) => {
+        setIsEdit(true);
+        setTodo(todo.todo);
+        setTempUidd(todo.uidd);
+    };
+
+
+    const handleEditConfirm = () => {
+        update(ref(db, `/${auth.currentUser.uid}/${tempUidd}`), {
+            todo: todo,
+            tempUidd: tempUidd
+        });
+
+        setTodo("");
+        setIsEdit(false);
+    };
+
     return (
-        <div>
-            <h1>Your Todo List</h1>
-            <button onClick={handleSignOut}>Signout</button>
+        <div className='homepage'>
+            <div class="row d-flex justify-content-center align-items-center h-100">
+                <div class="col">
+                    <div class="card" id="list1">
+                        <div className='todos'>
+                            <div class="card-body py-4 px-4 px-md-5">
+                                <div class="text-center pt-3 pb-2">
+                                    <img src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-todo-list/check1.webp"
+                                        alt="Check" width="60"></img>
+
+                                    <h2 class="my-4">Todo List</h2>
+                                </div>
+
+
+                                <input
+                                    className="add-edit-input"
+                                    type="text"
+                                    placeholder="Add todo..."
+                                    value={todo}
+                                    onChange={(e) => setTodo(e.target.value)}
+                                />
+                                {todos.map((todo) => (
+                                    <div className="todo">
+                                        <h1>{todo.todo}</h1>
+                                        <EditIcon
+                                            fontSize="medium"
+                                            onClick={() => handleUpdate(todo)}
+                                            className="edit-button"
+                                        />
+                                        <DeleteIcon
+                                            fontSize="medium"
+                                            onClick={() => handleDelete(todo.uidd)}
+                                            className="delete-button"
+                                        />
+                                    </div>
+                                ))}
+
+                                {isEdit ? (
+                                    <div>
+                                        <CheckIcon onClick={handleEditConfirm} className="add-confirm-icon" />
+                                    </div>
+                                ) : (
+                                    <div>
+                                        <AddIcon onClick={writeToDatabase} className="add-confirm-icon" />
+                                    </div>
+                                )}
+                                <div className='logout-icon'>
+                                    <button onClick={handleSignOut} type="button" class="btn btn-default btn-sm">
+                                        <span class="glyphicon glyphicon-log-out"></span> Log out
+                                    </button>
+                                </div>
+
+
+                            </div>
+
+                        </div>
+                        {/* <ExitToAppTwoToneIcon  className="logout-icon" /> */}
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,12 +1,23 @@
 import React, { useState, useEffect } from 'react';
-import { signInWithEmailAndPassword, onAuthStateChanged } from "firebase/auth";
+import { signInWithEmailAndPassword, onAuthStateChanged, createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../firebase.js";
 import { useNavigate } from 'react-router-dom';
+import './welcome.css'
+import TodoSVG from '../static/login_background.svg'
+
+
 
 export default function Welcome() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    const [isRegistering, setIsRegistering] = useState(false);
+    const [registerInformation, setRegisterInformation] = useState({
+        email: "",
+        confirmEmail: "",
+        password: "",
+        confirmPasssword: ""
+    });
 
     // if user is signed in we want to keep them on homepage 
     useEffect(() => {
@@ -18,12 +29,12 @@ export default function Welcome() {
     }, []);
 
 
-    const handleEmailChange = (email) => {
-        setEmail(email.target.value);
+    const handleEmailChange = (e) => {
+        setEmail(e.target.value);
     }
 
-    const handlePasswordChange = (password) => {
-        setPassword(password.target.value);
+    const handlePasswordChange = (p) => {
+        setPassword(p.target.value);
     }
 
     // if user has given correct email password take them to their homepage 
@@ -33,18 +44,60 @@ export default function Welcome() {
             navigate('/homepage')
         }).catch((err) => alert(err.message));
     }
+    const handleRegister = () => {
+        if (registerInformation.email != registerInformation.confirmEmail) {
+            alert("Email does not match")
+            return
+        } else if (registerInformation.password != registerInformation.confirmPasssword) {
+            alert("Password does not match")
+            return
+        }
+        createUserWithEmailAndPassword(auth, registerInformation.email, registerInformation.password).then(() => {
+            navigate('/homepage');
+        })
+            .catch((err) => alert(err.message));
+    };
 
     return (
         <div className="welcome">
+            <img src={TodoSVG} className="todo-svg" />
             <h1>Todo List</h1>
-            <div className="login-container">
-                <input type="email" onChange={handleEmailChange} value={email} />
-                <input type="password" onChange={handlePasswordChange} value={password} />
-                <button onClick={handleSignIn}>Sign In</button>
-                <a href=""> Create an account</a>
-            </div>
+
+            {isRegistering ? (
+                <>
+                    <div className="register-container">
+                        <h2 class="fw-bold mb-2 text-uppercase">Sign Up Now</h2>
+                        <input type="email" placeholder='Email' value={registerInformation.email}
+                            onChange={(e) => setRegisterInformation({ ...registerInformation, email: e.target.value })} />
+                        <input type="email" placeholder='Confirm Email' value={registerInformation.confirmEmail}
+                            onChange={(e) => setRegisterInformation({ ...registerInformation, confirmEmail: e.target.value })} />
+                        <input type="password" placeholder='Password' value={registerInformation.password}
+                            onChange={(p) => setRegisterInformation({ ...registerInformation, password: p.target.value })} />
+                        <input type="password" placeholder='Confirm Password' value={registerInformation.confirmPasssword}
+                            onChange={(p) => setRegisterInformation({ ...registerInformation, confirmPasssword: p.target.value })} />
+                        <div className='register-button'>
+                            <button class="btn btn-dark btn-lg btn-block" onClick={handleRegister}>Register</button>
+                        </div>
+                        <button className="create-account-button" onClick={() => setIsRegistering(false)}>Go Back</button>
+                    </div>
+                </>
+            ) : (
+                <>
+                    <div className='login-container'>
+                        <h2 class="fw-bold mb-2 text-uppercase">Login</h2>
+                        <p class="fw-normal mb-3 pb-3" >Please enter your login and password!</p>
+                        <input class="form-label" type="email" placeholder='Email' onChange={handleEmailChange} value={email} />
+                        <input class="form-label" type="password" placeholder='Password' onChange={handlePasswordChange} value={password} />
+                        <div className="sign-in-button">
+                            <button class="btn btn-dark btn-lg btn-block" onClick={handleSignIn}>Sign In</button>
+                        </div>
+                        <button className="create-account-button" onClick={() => setIsRegistering(true)}> Create an account </button>
+                    </div>
+                </>
+            )}
+
 
         </div>
 
-    )
+    );
 }
